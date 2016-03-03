3743efb940623668c1bb9f2ae43207db091406f7
zhjch05 <zjc1996@brandeis.edu>
2016-03-03 13:33:47 -0500

    customized signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
   get 'login/index'
   root to: 'login#index'
