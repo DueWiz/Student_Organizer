235f4219c3200334a475bafeaabbba8379cc637e
XingyuLu <rmsylar@gmail.com>
2016-03-03 15:59:47 -0500

    group/hw/userg/userh

---
+++ app/controllers/homework_controller.rb
@@ -0,0 +1,19 @@
+class HomeworkController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'homework/index'
+
+  get 'homework/new'
+
+  get 'homework/edit'
+
+  get 'homework/create'
+
+  get 'homework/update'
+
+  get 'homework/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
