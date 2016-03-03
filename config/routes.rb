abc883bd3b8b1bdbc958dab89ebeeda494f0c40c
zhjch05 <zjc1996@brandeis.edu>
2016-03-03 13:41:35 -0500

    rename login to welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
-  get 'login/index'
-  root to: 'login#index'
+  root to: 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
