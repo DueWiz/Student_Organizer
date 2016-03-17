2b51f83f5e057156a20c16baa4792149649621ee
Nishan Acharya <nishan.acharya@gmail.com>
2016-03-17 18:07:15 -0400

    fixed group modal

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20160317215502) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
+    t.boolean  "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
