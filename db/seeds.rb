d873111417def3b94d88edffe9230765d6d12a11
zhengyr <stella.zhengyr@gmail.com>
2016-03-17 16:02:21 -0400

    basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Homework.delete_all
+Group.delete_all
+UserHomework.delete_all
+
+user = User.create! :email => 'zheng@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+group = Group.create(name: "cosi166")
+homework = Homework.create(name: "maze", due_date: DateTime.new(2001,-11,-26,-20,-55,-54,'+7'), description: "this is the desp",
+  professor: "pito", group_id: group.id)
+UserHomework.create(status: "done", grade: 100, comment: "good job", user_id: user.id,
+  homework_id: homework.id, note: "this is note")
