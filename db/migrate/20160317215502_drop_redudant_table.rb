abc1ab7eaa571d0ba7d64371f5b63a7bb9035887
zhjch05 <zjc1996@brandeis.edu>
2016-03-17 17:59:23 -0400

    remove redundant tables

---
+++ db/migrate/20160317215502_drop_redudant_table.rb
@@ -0,0 +1,6 @@
+class DropRedudantTable < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :group_users
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317214606) do
-
-  create_table "group_users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160317215502) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
