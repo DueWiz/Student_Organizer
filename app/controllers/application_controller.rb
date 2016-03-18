163ce91eddda3608d80cb885cfb1aac27f958dbd
zhjch05 <zjc1996@brandeis.edu>
2016-03-18 00:13:48 -0400

    helpers/controllers/layouts improvement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+
+  helper_method :user_groups
+
+  def user_groups
+    if user_signed_in?
+      current_user.groups
+    else
+      []
+    end
+  end
 end
