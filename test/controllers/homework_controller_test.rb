88c9782a51b08514e461aec6c5129bd2d5485313
zhengyr <stella.zhengyr@gmail.com>
2016-03-29 21:20:50 -0400

    remove controller test

--- test/controllers/homework_controller_test.rb
+++ test/controllers/homework_controller_test.rb
@@ -1,34 +1,34 @@
 require 'test_helper'
 
 class HomeworkControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get homework_index_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get homework_new_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get homework_edit_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get homework_create_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get homework_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get homework_destroy_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get homework_index_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get new" do
+  #   get homework_new_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get edit" do
+  #   get homework_edit_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get create" do
+  #   get homework_create_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get update" do
+  #   get homework_update_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get destroy" do
+  #   get homework_destroy_url
+  #   assert_response :success
+  # end
 
 end
--- test/controllers/user_homework_controller_test.rb
+++ test/controllers/user_homework_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UserHomeworkControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get user_homework_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get user_homework_index_url
+  #   assert_response :success
+  # end
 
 end
