f75ca244a8d9451c4dc23329fc5196c396872dd3
Jincheng Zhang <zjc1996@brandeis.edu>
2016-03-31 16:02:45 -0400

    remove test and create js

--- app/controllers/latte_controller.rb
+++ app/controllers/latte_controller.rb
@@ -8,6 +8,7 @@ class LatteController < ApplicationController
             if @latte.save
                 format.html { redirect_to welcome_index_path, notice: 'latte completed.' }
                 format.json { render json: @latte, status: :created, location: @latte }
+                #format.js { render :file => "/path/to/save.js.erb" }
             else
                 format.html { redirect_to homework_index_path, notice: 'latte not completed.' }
                 format.json { render json: @latte.errors, status: :unprocessable_entity }
--- test/controllers/latte_controller_test.rb
+++ test/controllers/latte_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class LatteControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get latte_create_url
-    assert_response :success
-  end
 
 end
