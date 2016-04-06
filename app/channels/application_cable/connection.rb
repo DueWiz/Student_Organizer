93f20b2aacfc5303b137d3130e3a6c3ae43d5846
zhjch05 <zjc1996@brandeis.edu>
2016-04-06 03:02:54 -0400

    warden cookies for auth with cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,15 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 module ApplicationCable
-  class Connection < ActionCable::Connection::Base
-  end
+    class Connection < ActionCable::Connection::Base
+
+  protected
+    def find_verified_user
+        verified_user = User.find_by(id: cookies.signed['user.id'])
+        if verified_user && cookies.signed['user.expires_at'] > Time.now
+            verified_user
+        else
+            reject_unauthorized_connection
+        end
+    end
+    end
 end
