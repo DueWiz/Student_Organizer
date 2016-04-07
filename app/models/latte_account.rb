e506023cbfbe27050d49d3b71f6bae7f3dbd8ded
Jincheng Zhang <zjc1996@brandeis.edu>
2016-04-07 14:47:51 -0400

    attr_encrypted 2

--- app/models/latte_account.rb
+++ app/models/latte_account.rb
@@ -1,10 +1,11 @@
 class LatteAccount < ApplicationRecord
 
   validates :name, :user_id, presence: true
+  belongs_to :user
   attr_encrypted :password, key: :encryption_key
   def encryption_key
       SecureRandom.base64(32)
   end
 
-  belongs_to :user
+
 end
