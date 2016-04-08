768699a5adaa63ba04b206edd0aec2c475680adb
zhjch05 <zjc1996@brandeis.edu>
2016-04-07 21:55:16 -0400

    sidekiq done, /latte/info for loading demo

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 redis: redis-server
 actioncable: bundle exec puma -p 28080  cable/config.ru
 web: bundle exec puma -p 3000  ./config.ru
+sidekiq: bundle exec sidekiq
--- app/jobs/load_latte_job.rb
+++ app/jobs/load_latte_job.rb
@@ -1,7 +1,38 @@
 class LoadLatteJob < ApplicationJob
-  queue_as :urgent
+  queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(current_user)
+    ActionCable.server.broadcast "latte_info_#{current_user.id}",
+      latte_info: 'good'
+    agent = Mechanize.new
+    agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    logger.info "Latte Account Name:#{current_user.latte_account.name}"
+    logger.info "Latte Account password:#{current_user.latte_account.password}\n\n\n\n\n\n\n\n\n"
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: 'Initialized'
+    page = agent.get('https://login.brandeis.edu/?factors=BRANDEIS.EDU,ldapauth&cosign-moodle2-prod&https://moodle2.brandeis.edu/')
+    form = page.form('f')
+    form.field_with(:id => "login").value = current_user.latte_account.name
+    form.field_with(:id => "password").value = current_user.latte_account.password
+    page = agent.submit(form, form.buttons_with(:type => /submit/)[0])
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: 'Logged in'
+
+
+
+
+    page.links_with(css: "a.course_show").each do |link|
+      if link.text =~ /^161/
+        course_page = link.click
+        course_page.links_with(css: "li.assign div.activityinstance a").each do |a_link|
+          assignment_page = a_link.click
+          a_title = assignment_page.xpath("//div[@role='main']/h2/text()")
+          ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: "#{a_title}"
+          a_sub_status = assignment_page.xpath("//td[contains(., 'Submission status')]/following-sibling::td/text()")
+          ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: "#{a_sub_status}"
+          a_due_date = assignment_page.xpath("//td[contains(., 'Due date')]/following-sibling::td/text()")
+          ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: "#{a_due_date}"
+          ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: "Entered assignment page"
+        end
+      end
+    end
   end
 end
