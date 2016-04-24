d540458479826b5b3a5fb8b3a2f33bd4f9a985f0
Jincheng Zhang <zjc1996@brandeis.edu>
2016-04-24 15:41:50 -0400

    add info alert

--- app/jobs/load_latte_job.rb
+++ app/jobs/load_latte_job.rb
@@ -2,12 +2,12 @@ class LoadLatteJob < ApplicationJob
   queue_as :default
 
   def perform(current_user)
-    ActionCable.server.broadcast "latte_info_#{current_user.id}",
-      latte_info: 'good'
     agent = Mechanize.new
     agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     logger.info "Latte Account Name:#{current_user.latte_account.name}"
     logger.info "Latte Account password:#{current_user.latte_account.password}\n\n\n\n\n\n\n\n\n"
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", spin_status: 'active'
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", bar_status: 'Initialized'
     ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: 'Initialized'
     page = agent.get('https://login.brandeis.edu/?factors=BRANDEIS.EDU,ldapauth&cosign-moodle2-prod&https://moodle2.brandeis.edu/')
     form = page.form('f')
@@ -15,6 +15,7 @@ class LoadLatteJob < ApplicationJob
     form.field_with(:id => "password").value = current_user.latte_account.password
     page = agent.submit(form, form.buttons_with(:type => /submit/)[0])
     ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: 'Logged in'
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", bar_status: 'Logged in'
 
 
     hw_count = 0
@@ -26,8 +27,10 @@ class LoadLatteJob < ApplicationJob
         end
       end
     end
-    ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: "Expect to load #{hw_count} items."
     hw_count2 = 0
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: "Expect to load #{hw_count} items."
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", bar_status: 'Loading'
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", progress: "#{hw_count2}/#{hw_count} items"
     page.links_with(css: "a.course_show").each do |link|
       if link.text =~ /^161/
         course_page = link.click
@@ -39,6 +42,10 @@ class LoadLatteJob < ApplicationJob
           #Associations with User
           u_h = UserHomework.new
           u_h.user_id = current_user.id
+
+          ActionCable.server.broadcast "latte_info_#{current_user.id}", spin_status: 'active'
+          ActionCable.server.broadcast "latte_info_#{current_user.id}", bar_status: 'Loading'
+          ActionCable.server.broadcast "latte_info_#{current_user.id}", progress: "#{hw_count2}/#{hw_count} items"
           #Homework stats
           ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: "Now loading #{hw_count2}/#{hw_count} items."
           a_title = assignment_page.xpath("//div[@role='main']/h2/text()")
@@ -62,5 +69,7 @@ class LoadLatteJob < ApplicationJob
       end
     end
     ActionCable.server.broadcast "latte_info_#{current_user.id}", latte_info: '<br/><a href="/">Back</a>'
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", spin_status: 'inactive'
+    ActionCable.server.broadcast "latte_info_#{current_user.id}", bar_status: "Finished"
   end
 end
