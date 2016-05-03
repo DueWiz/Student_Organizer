9610c289772bf590051dc767d06ba3a8b7b9fb45
XingyuLu <rmsylar@gmail.com>
2016-05-03 16:10:15 -0400

    calendar debug

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,5 +1,10 @@
 class CalendarController < ApplicationController
   def index
-  	@homeworks = Homework.all
+  	myhomework = current_user.user_homeworks
+    myhwArray = Array.new
+    myhomework.each do |hw|
+      myhwArray.push(hw.homework_id)
+    end
+    @homeworks = Homework.where(id: myhwArray)
   end
 end
