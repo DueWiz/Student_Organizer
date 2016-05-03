08cde753eb51201759ce123da12160e2a6540a71
Jincheng Zhang <zjc1996@brandeis.edu>
2016-05-03 11:20:21 -0400

    sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redistogo:efedf4614c6c9b98667075aea4bbaa4e@hoki.redistogo.com:9079/5', namespace: "student_organizer" }
+  config.redis = { url: 'redis://redistogo:efedf4614c6c9b98667075aea4bbaa4e@hoki.redistogo.com:9079/', namespace: "student_organizer" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redistogo:efedf4614c6c9b98667075aea4bbaa4e@hoki.redistogo.com:9079/5', namespace: "student_organizer" }
+  config.redis = { url: 'redis://redistogo:efedf4614c6c9b98667075aea4bbaa4e@hoki.redistogo.com:9079/', namespace: "student_organizer" }
 end
