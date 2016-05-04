ec17f59bdb98aa97bd15706ae0ec927e5a5e4b9b
Jincheng Zhang <zjc1996@brandeis.edu>
2016-05-04 12:58:35 -0400

    mount path

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module StudentOrganizer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
+    config.action_cable.mount_path = '/cable'
   end
 end
